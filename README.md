c4d2644e027760e2108c9852d7f645ee9de0dd07
Gabriel Torres <gabriel.torres@sou.inteli.edu.br>
2023-12-15 15:00:29 -0300

    prova feita amigos e amigas

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# 20232BSET03P2
-Inteli - Engenharia de Software | Avaliação 2023-2B P2
+# PROVA 2 DO GABS
+
+## Sanitizar e validar dados de entrada para evitar SQL Injection.
+
+O que tava rolando? A chamada API que o professor forneceu estava inserindo diretamente o nome da string SQL sem qualquer verificação! Isso torna o código vulnerável a ataques de injeção de SQL. 
+
+O que fiz para resolver? Utilizando o placeholder (?) eu passo o valor do nome como um parâmetro no array, o que impede que o db leia uma possível injeção de SQL como SQL.
+
+Ja na parte do animaltype o que fiz foi fazer uma verificação. Agora o sistema só aceita estar escrito em animaltype as palavras 'cats' ou 'dogs'!
+
+## Corrigir a lógica de votação para que verifique se o registro do animal existe antes de adicionar um voto.
+
+O que tava rolando? Não havia nenhum tipo de verificação na parte de votos! Além disso o ID estava sempre vindo como undefined na hora de fazer os gets.
+
+O que fiz para resolver? Mudei as tabelas para id ser INTEGER PRIMARY KEY AUTOINCREMENT. Além disso, agora ele faz a verificação no banco se o id realmente existe. Caso não, o usuário toma um erro.
+<img src="assets/id_nao_existente.png">
+
+## Implementar e tratar erros de maneira adequada, sem vazar detalhes de implementação.
+
+Os erros foram tratados como ditos anteriormente. Para evitar vazar detalhes de implementação, deixei os logs de erro os mais simples possíveis.
+Sendo eles: Chamada incorreta ou Erro interno da aplicação.
+
+## Implementar todos os métodos que possuem assinatura no código
+Métodos que estavam vazios agora estão implementados!
+
+
+### Prints de algumas chamadas!
+Chamadas bem sucedidas
+<img src="assets/chamada_bem_sucedida_cats.png">
+<img src="assets/chamada_bem_sucedida_dogs.png">
+
+Chamadas mal sucedidas
+<img src="assets/chamada_mal_sucedida_cats.png">
+<img src="assets/chamada_mal_sucedida_dogs.png">
+<img src="assets/chamada_incorreta_votos.png">
--- index.js
+++ index.js
@@ -11,35 +11,98 @@ app.use(bodyParser.json());
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  // o id estava ficando undefined, para ajustar a lógica coloquei o id como PRIMARY KEY e AUTOINCREMENT   
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
 });
 
 app.post('/cats', (req, res) => {
-  const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
-    if (err) {
-      res.status(500).send("Erro ao inserir no banco de dados");
-    } else {
-      res.status(201).json({ id: this.lastID, name, votes: 0 });
+    const name = req.body.name;
+
+    // Verificação se o usuário mando um campo chamado 'nome', garantindo que caso ele tente 
+    // mandar qualquer outra coisa sem ser o nome, não será aceito nem lido pelo sistema.
+    // resolvi não falar pro usuário que era o nome para não vazar detalhes da implementação
+
+    if (!name) {
+      return res.status(400).send("Chamada incorreta.");
     }
+  
+    // coloquei um placeholder onde inseria diretamente o nome na string SQL,
+    // pois isso torna o código vulnerável a ataques de injeção de SQL. 
+    // utilizando o placeholder eu passo o valor do nome como um parâmetro no array,
+    // o que impede que o db leia uma possível injeção de SQL como SQL.
+    db.run("INSERT INTO cats (name, votes) VALUES (?, 0)", [name], function (err) {
+      if (err) {
+        console.error(err);
+        // aqui tenho um erro ao inserir no banco de dados, para o usuário vou mandar apenas erro interno    
+        return res.status(500).send("Erro interno da aplicação");
+      } else {
+        res.status(201).json({ id: this.lastID, name, votes: 0 });
+      }
+    });
   });
-});
+  
 
 app.post('/dogs', (req, res) => {
+  // mesma lógica aplicada para cats
+  const name = req.body.name;
+    if (!name) {
+      return res.status(400).send("Chamada incorreta.");
+    }
   
-});
+    db.run("INSERT INTO dogs (name, votes) VALUES (?, 0)", [name], function (err) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Erro interno da aplicação");
+      } else {
+        res.status(201).json({ id: this.lastID, name, votes: 0 });
+      }
+    });
+  });
 
 app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
-});
+    const { animalType, id } = req.params;
 
+    // verifico se o animaltype é cats | dogs, que são as minhas duas tabelas
+    // caso o usuário coloque qualquer coisa diferente ele dará o log de 
+    // chamada incorreta.
+
+    if (animalType !== 'cats' && animalType !== 'dogs') {
+      return res.status(404).send("Chamada incorreta.");
+  }
+
+    const tableName = animalType;
+
+    // lógica do placeholder implementada aqui também! 
+    // note que o tablename foi verificado anteriormente, logo evitando qualquer
+    // outro tipo de inserção sem ser cats ou dogs
+
+    db.get(`SELECT * FROM ${tableName} WHERE id = ?`, [id], (err, row) => {
+      if (err) {
+        return res.status(500).send("Erro interno da aplicação");
+      }
+  
+      if (!row) {
+        return res.status(404).send("Chamada incorreta.");
+      }
+
+  
+      // atualização do voto
+      // lembre-se que o tablename foi verificado anteriormente
+
+      db.run(`UPDATE ${tableName} SET votes = votes + 1 WHERE id = ?`, [id], (err) => {
+        if (err) {
+          return res.status(500).send("Erro interno da aplicação");
+        }
+        res.status(200).send("Voto computado");
+      });
+    });
+  });
+  
 app.get('/cats', (req, res) => {
   db.all("SELECT * FROM cats", [], (err, rows) => {
     if (err) {
-      res.status(500).send("Erro ao consultar o banco de dados");
+      res.status(500).send("Erro interno da aplicação");
     } else {
       res.json(rows);
     }
@@ -47,7 +110,13 @@ app.get('/cats', (req, res) => {
 });
 
 app.get('/dogs', (req, res) => {
-  
+  db.all("SELECT * FROM dogs", [], (err, rows) => {
+    if (err) {
+      res.status(500).send("Erro interno da aplicação");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
